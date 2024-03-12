01a642909dfdbbfb0f4c7dc87623d41422c8f5c8
Sebastien Joseph <sebastienjoseph07@outlook.com>
2024-03-11 20:19:29 -0400

    Updated confgig with Bean for jwtDecoder under class JwtDecoder.

---
+++ src/main/java/com/shopsmart/apigateway/config/JwtDecoder.java
@@ -0,0 +1,19 @@
+package com.shopsmart.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+
+@Configuration
+public class JwtDecoder {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        return ReactiveJwtDecoders.fromIssuerLocation(issuerUri);
+    }
+}
--- src/main/java/com/shopsmart/apigateway/config/SecurityConfig.java
+++ src/main/java/com/shopsmart/apigateway/config/SecurityConfig.java
@@ -5,21 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebFluxSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final RSAPublicKey key;
+    private final ReactiveJwtDecoder reactiveJwtDecoder;
 
     @Bean
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         http
                 .csrf(csrf -> csrf.disable())  // Disable CSRF protection (for testing purposes)
                 .authorizeExchange(request -> request
@@ -27,14 +24,9 @@ public class SecurityConfig {
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oAuth2 -> oAuth2
-                        .jwt(jwt -> jwt.jwtDecoder(jwtDecoder()))
+                        .jwt(jwt -> jwt.jwtDecoder(reactiveJwtDecoder))
                 );
 
         return http.build();
-    }
-
-    @Bean
-    protected ReactiveJwtDecoder jwtDecoder() {
-        return NimbusReactiveJwtDecoder.withPublicKey(this.key).build();
     }
 }
